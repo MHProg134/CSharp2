2f5c03c91544717be2eaadc5ee2495f6ddab9b59
MDProg134 <MDProg134@gmail.com>
2019-04-16 01:14:17 -0700

    Homework 1 - Mark Hill

---
+++ Hello World/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmit();
+        }
+
+        private void UxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmit();
+        }
+
+        private void EnableSubmit()
+        {
+            if ((uxName.Text != "") & (uxPassword.Text != ""))
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+    }
+}
